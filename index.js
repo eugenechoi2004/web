c9a7077227eea522abdb2342b7e393ba805f0634
eugenechoi2004 <35019459+eugenechoi2004@users.noreply.github.com>
2022-02-15 13:22:16 -0500

    finished lab2

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ app.get('/list_render', function(req,res){
 });
 
 app.get('/madlib',function(req,res){
-
     return res.render('madlib_template')
 })
 
@@ -74,6 +73,35 @@ app.post('/madlib',function(req,res){
     return res.render('madlib_template',req.body)
 })
 
+app.get('/number/:digit',function(req,res){
+    console.log(req.params.digit)
+    digit = req.params.digit
+    if (!isPositiveInteger(digit)){
+        return res.render('invalid_template')
+    }
+    obj = {'digit':digit,
+           'squared': Math.pow(digit,2),
+            'cubed': Math.pow(digit,3),
+            'power4': Math.pow(digit,4)
+        }
+    if (req.query['format']=='json'){
+        return res.json(obj)
+    }
+    return res.render('number_template', obj)
+})
+function isPositiveInteger(str) {
+    if (typeof str !== 'string') {
+      return false;
+    }
+  
+    const num = Number(str);
+  
+    if (Number.isInteger(num) && num > 0) {
+      return true;
+    }
+  
+    return false;
+  }
 // -------------- listener -------------- //
 // // The listener is what keeps node 'alive.' 
 
