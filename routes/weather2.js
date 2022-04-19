162abbc148cc168c88600c914bbb812dd3a005a1
root <eugenechoi2004@gmail.com>
2022-04-19 13:50:24 -0400

    start of wordle

--- routes/weather2.js
+++ routes/weather2.js
@@ -39,42 +39,71 @@ function valid(req,res,next) {
 }
  
 function stepTwo(req,res,next) {
-  
-    https.get(res.locals.url,options, function(response) {
-      var rawData = '';
-      response.on('data', function(chunk) {
-        rawData += chunk;
-      });
-      response.on('end', function() {
-        var obj = JSON.parse(rawData);
-        url = obj.properties.forecast
-        res.locals.url = url
-        next()
-      });
- })
+    try{
+        https.get(res.locals.url,options, function(response) {
+          var rawData = '';
+          response.on('data', function(chunk) {
+            rawData += chunk;
+          });
+          response.on('end', function() {
+            try{
+                var obj = JSON.parse(rawData);
+                url = obj.properties.forecast
+                res.locals.url = url
+                next()
+            }
+            catch{
+                return res.render('weather_template',{message:"Try Again!"})
+            }
+          });
+        })
+    }
+    catch{
+        return res.render('weather_template',{message:"Try Again!"})
+    }
     
 }
 
 function stepThree(req,res,next) {
-    console.log('Start of Step Three')
-    console.log(res.locals.url)
-    https.get(res.locals.url,options, function(response) {
-      var rawData = '';
-      response.on('data', function(chunk) {
-        rawData += chunk;
-      });
-      response.on('end', function() {
-        var obj = JSON.parse(rawData);
-        var periods = obj.properties.periods
-        for (i=0; i < periods.length; i++) {
-            console.log(periods[i])
+    try{
+        console.log('Start of Step Three')
+        console.log(res.locals.url)
+        if (res.locals.url!=null){
+            https.get(res.locals.url,options, function(response) {
+              var rawData = '';
+              response.on('data', function(chunk) {
+                rawData += chunk;
+              });
+              response.on('end', function() {
+                var obj = JSON.parse(rawData);
+                var periods = obj.properties.periods
+                console.log(periods[0]['icon'])
+                console.log(periods[0]['shortForecast'])
+                var output = {
+                    message:'Success!',
+                    icon : periods[0]['icon'],
+                    temp : periods[0]['temperature'],
+                    forecast : periods[0]['shortForecast']
+                }
+                res.locals.output = output
+                next()
+              });
+            })
         }
-      });
- })
-    next()
+        else{
+            return res.render('weather_template',{message:"Try Again!"})
+        }
+    }
+    catch{
+        return res.render('weather_template',{message:"Try Again!"})
+    }
+}
+
+function stepFour(req,res,next) {
+    return res.render('weather_template',res.locals.output)
 }
  
-app.get('/getweather', [valid, stepTwo, stepThree],function(req,res){
+app.get('/getweather', [valid, stepTwo, stepThree,stepFour],function(req,res){
     return res.render('weather_template')
 })
 
